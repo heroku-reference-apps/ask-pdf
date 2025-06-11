26154e35c395718cc7993c054075a1462cfef3a4
Julian Duque <julianduquej@gmail.com>
2025-06-11 15:35:08 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 import 'dotenv/config';
-import url from 'node:url';
-import path from 'node:path';
 import fastify from 'fastify';
-import fastifyStatic from '@fastify/static';
 import formbody from '@fastify/formbody';
 import multipart from '@fastify/multipart';
 import { reactRouterFastify } from '@mcansh/remix-fastify/react-router';
 
 async function main() {
-  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
   const app = fastify({
     logger: {
       level: process.env.LOG_LEVEL || 'info',
@@ -19,25 +15,7 @@ async function main() {
   await app.register(formbody);
   await app.register(multipart);
 
-  // Serve static assets from 'build/client/assets' directory.
-  // These are fingerprinted, so they can be cached forever.
-  await app.register(fastifyStatic, {
-    root: path.resolve(__dirname, 'build/client/assets'),
-    prefix: '/assets',
-    wildcard: false,
-    immutable: true,
-    maxAge: '1y',
-  });
-
-  // Serve other static assets from 'public' folder.
-  await app.register(fastifyStatic, {
-    root: path.resolve(__dirname, 'public'),
-    prefix: '/',
-    wildcard: false,
-    decorateReply: false,
-  });
-
-  // Attach the React Router handler.
+  // Attach the React Router handler, which will also handle serving static assets.
   await app.register(reactRouterFastify);
 
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
